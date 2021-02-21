afeb2fb44e8f39e0762b7a76be09c9d4c0604888
ArmandoPrado <73237293+ArmandoPrado@users.noreply.github.com>
2021-02-21 13:13:13 -0600

    Create Login.php
    Cascaron de login

---
+++ public_html/Login.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="icon" type="image/png" href="assets/Logos/LogoIcono.png" />
+    <title> Workick | Login </title>
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet">
+    <!-- Toastr style -->
+    <link href="assets/css/plugins/toastr/toastr.min.css" rel="stylesheet">
+    <!-- Gritter -->
+    <link href="assets/js/plugins/gritter/jquery.gritter.css" rel="stylesheet">
+    <link href="assets/css/animate.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="assets/css/plugins/footable/footable.core.css" rel="stylesheet">
+    <link href="assets/css/plugins/chosen/bootstrap-chosen.css" rel="stylesheet">
+    <link href="assets/css/plugins/daterangepicker/daterangepicker-bs3.css" rel="stylesheet">
+    <link href="assets/css/plugins/datapicker/datepicker3.css" rel="stylesheet">
+
+    <script src="assets/js/fn/Principal.js"></script>
+
+    <script src="assets/js/fn/notificacion.js"></script>
+
+
+</head>
+
+<body class="gray-bg">
+
+    <div class="loginColumns animated fadeInDown">
+        <div class="row">
+
+            <div class="col-md-6">
+                <h2 class="font-bold">Welcome to Workick</h2>
+
+                <p>
+                    Perfectly designed and precisely prepared admin theme with over 50 pages with extra new web app views.
+                </p>
+
+                <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+                </p>
+
+                <p>
+                    When an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                </p>
+
+                <p>
+                    <small>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</small>
+                </p>
+
+            </div>
+            <div class="col-md-6">
+                <div class="ibox-content">
+                    <form class="m-t" role="form" action="index.html">
+                        <div class="form-group">
+                            <input type="email" class="form-control" placeholder="Username" required="">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" placeholder="Password" required="">
+                        </div>
+                        <button type="submit" class="btn btn-primary block full-width m-b">Login</button>
+
+                        <a href="#">
+                            <small>Forgot password?</small>
+                        </a>
+
+                        <p class="text-muted text-center">
+                            <small>Do not have an account?</small>
+                        </p>
+                        <a class="btn btn-sm btn-white btn-block" href="register.html">Create an account</a>
+                    </form>
+                    <p class="m-t">
+                        <small>Workick &copy; 2014</small>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <hr />
+        <div class="row">
+            <div class="col-md-6">
+                Copyright Example Company
+            </div>
+            <div class="col-md-6 text-right">
+                <small>© 2014-2015</small>
+            </div>
+        </div>
+    </div>
+
+    <script src="assets/js/jquery-3.1.1.min.js"></script>
+    <!-- jQuery UI -->
+    <script src="assets/js/plugins/jquery-ui/jquery-ui.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/plugins/metisMenu/jquery.metisMenu.js"></script>
+    <script src="assets/js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+    <!-- Flot -->
+    <script src="assets/js/plugins/flot/jquery.flot.js"></script>
+    <script src="assets/js/plugins/flot/jquery.flot.tooltip.min.js"></script>
+    <script src="assets/js/plugins/flot/jquery.flot.spline.js"></script>
+    <script src="assets/js/plugins/flot/jquery.flot.resize.js"></script>
+    <script src="assets/js/plugins/flot/jquery.flot.pie.js"></script>
+    <!-- Peity -->
+    <script src="assets/js/plugins/peity/jquery.peity.min.js"></script>
+    <script src="assets/js/demo/peity-demo.js"></script>
+    <!-- Custom and plugin javascript -->
+    <script src="assets/js/inspinia.js"></script>
+    <script src="assets/js/plugins/pace/pace.min.js"></script>
+    <!-- GITTER -->
+    <script src="assets/js/plugins/gritter/jquery.gritter.min.js"></script>
+    <!-- Sparkline -->
+    <script src="assets/js/plugins/sparkline/jquery.sparkline.min.js"></script>
+    <!-- Sparkline demo data  -->
+    <script src="assets/js/demo/sparkline-demo.js"></script>
+    <!-- ChartJS-->
+    <script src="assets/js/plugins/chartJs/Chart.min.js"></script>
+    <!-- Toastr -->
+    <script src="assets/js/plugins/toastr/toastr.min.js"></script>
+    <script src="assets/js/plugins/footable/footable.all.min.js"></script>
+    <script src="assets/js/plugins/daterangepicker/daterangepicker.js"></script>
+    <script src="assets/js/plugins/datapicker/bootstrap-datepicker.js"></script>
+    <script src="assets/js/plugins/fullcalendar/moment.min.js"></script>
+    <script src="assets/js/plugins/dataTables/datatables.min.js"></script>
+    <!-- Chosen -->
+    <script src="assets/js/plugins/chosen/chosen.jquery.js"></script>
+
+</body>
+
+</html>
